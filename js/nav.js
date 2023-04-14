0a5232f81b2bd15c3985c79243eb1a7b821031e9
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-04-14 22:58:05 +0900

    JS:sub-menu

--- js/nav.js
+++ js/nav.js
@@ -20,4 +20,28 @@
   }
   searchIcon.addEventListener("click", handleSearchCon);
   closeCon.addEventListener("click", closeContainer);
+
+  // sub-menu
+  const gnbMenu = document.querySelector(".gnb-menu");
+  const gnbMenuElems = document.querySelectorAll(".nav-menu-a");
+  const subMenu = document.querySelector(".sub-menu");
+  const subBoxes = document.querySelectorAll(".nav-sub-box");
+
+  //gnb메뉴를 클릭하면 클릭된 타깃의 index출력
+  //타깃의 index와 같은 sub-box를 보여준다.
+  function handleMenu() {
+    console.log("click");
+    gnbMenu.classList.add = "Active";
+  }
+  gnbMenu.addEventListener("click", handleMenu);
 })();
+
+// for (let i = 0; i < gnbMenuElems.length; i++) {
+//   gnbMenuElems[i].dataset.index = i;
+//   subBoxes[i].dataset.index = i;
+
+//   console.log(event.target.dataset.index);
+//   if (event.target.dataset.index == subBoxes[i].dataset.index) {
+//     subBoxes[i].classList.add = "Over";
+//   }
+// }
