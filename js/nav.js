5a5e180f1b7dcd85595526193e3f079b6c84de10
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-04-14 19:28:58 +0900

    JS: search open/close

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,23 @@
 (() => {
+  // language
   const selectLang = document.querySelector(".select-language");
 
   function handleVisible() {
     selectLang.classList.toggle("show");
   }
   selectLang.addEventListener("click", handleVisible);
+
+  // search
+  const searchCon = document.querySelector(".search-container");
+  const searchIcon = document.querySelector(".h-open-search");
+  const closeCon = document.querySelector(".sch-close");
+
+  function handleSearchCon() {
+    searchCon.classList.add("show");
+  }
+  function closeContainer() {
+    searchCon.classList.remove("show");
+  }
+  searchIcon.addEventListener("click", handleSearchCon);
+  closeCon.addEventListener("click", closeContainer);
 })();
