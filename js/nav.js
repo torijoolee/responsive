64869ec942a91cab029de95714f48330bca9be4d
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-04-14 23:18:18 +0900

    JS:sub-menu

--- js/nav.js
+++ js/nav.js
@@ -23,25 +23,24 @@
 
   // sub-menu
   const gnbMenu = document.querySelector(".gnb-menu");
-  const gnbMenuElems = document.querySelectorAll(".nav-menu-a");
+  const gnbMenuItem = document.querySelectorAll(".nav-menu-a");
   const subMenu = document.querySelector(".sub-menu");
   const subBoxes = document.querySelectorAll(".nav-sub-box");
-
-  //gnb메뉴를 클릭하면 클릭된 타깃의 index출력
-  //타깃의 index와 같은 sub-box를 보여준다.
-  function handleMenu() {
-    console.log("click");
-    gnbMenu.classList.add = "Active";
+  function handleMenu(event) {
+    gnbMenu.classList.add("Active");
+    subMenu.classList.add("On");
+    for (let i = 0; i < gnbMenuItem.length; i++) {
+      gnbMenuItem[i].dataset.index = i;
+      subBoxes[i].dataset.index = i;
+      console.log(event.target.dataset.index);
+      const target = event.target.dataset.index;
+      if (target == subBoxes[i].dataset.index) {
+        subBoxes[i].classList.add("Over");
+      } else {
+        subBoxes[i].classList.remove("Over");
+      }
+    }
   }
+
   gnbMenu.addEventListener("click", handleMenu);
 })();
-
-// for (let i = 0; i < gnbMenuElems.length; i++) {
-//   gnbMenuElems[i].dataset.index = i;
-//   subBoxes[i].dataset.index = i;
-
-//   console.log(event.target.dataset.index);
-//   if (event.target.dataset.index == subBoxes[i].dataset.index) {
-//     subBoxes[i].classList.add = "Over";
-//   }
-// }
