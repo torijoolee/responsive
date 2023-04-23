c8dfaf5ef9da450c298954fdda12cfc9257d88a9
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-04-23 15:34:34 +0900

    toggleMenu

--- js/media.js
+++ js/media.js
@@ -2,19 +2,13 @@
   const mobNavBtn = document.querySelector(".mov-nav-btn");
   const mobNav = document.querySelector(".mob-nav");
   const bg = document.querySelector(".mob-nav .bg");
-  // 메뉴타이틀
+  // 메뉴타이틀 On
   const menuTitle = document.querySelectorAll(".m-nav-list-tit");
-  //타이틀 안에 있는 리스트
+  //타이틀 안에 있는 리스트 //0 => 100
   const innerMenu = document.querySelectorAll(".m-nav-list li dl");
-  const icon = document.querySelectorAll(".icon-nav");
   const searchIcon = document.querySelector(".h-open-search");
   const main = document.querySelector("#main");
   const searchCon = document.querySelector(".search-container");
-
-  for (let i = 0; i < menuTitle.length; i++) {
-    menuTitle[i].dataset.index = i;
-    innerMenu[i].dataset.index = i;
-  }
 
   //nav open
   function handleMobileNav() {
@@ -26,15 +20,23 @@
   }
   //accordion
   function handleMenuItem(event) {
-    for (let i = 0; i < menuTitle.length; i++) {
-      menuTitle[i].classList.remove("On");
-      innerMenu[i].style.display = "none";
-      if (event.target.classList.contains("On")) {
-        event.target.nextElementSibling.style.display = "block";
-        console.log(event.target.nextElementSibling);
-      } else {
-        event.target.classList.add("On");
-      }
+    let before = document.querySelector(".m-nav-list-tit.On");
+    let current = event.target;
+    if (current == null) return;
+    if (current.nextElementSibling == null) {
+      current = current.parentNode;
+    }
+    if (before && document.querySelector(".m-nav-list-tit.On") != current) {
+      before.nextElementSibling.style.maxHeight = null;
+      before.classList.remove("On");
+    }
+    current.classList.add("On");
+    let panel = current.nextElementSibling;
+    if (panel.style.maxHeight != 0) {
+      panel.style.maxHeight = null;
+      current.classList.remove("On");
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   }
 
@@ -42,6 +44,7 @@
     menu.addEventListener("click", handleMenuItem);
   });
   mobNavBtn.addEventListener("click", handleMobileNav);
+
   bg.addEventListener("click", closeNav);
   function blurMode() {
     main.classList.add("On");
